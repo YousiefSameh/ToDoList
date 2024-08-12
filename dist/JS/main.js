9f736bc49edeb99af15c1b17a48d6242220a02e8
Yousief Sameh <yousief.sameh@outlook.com>
2024-08-13 01:49:10 +0300

    1.2.0

--- dist/JS/main.js
+++ dist/JS/main.js
@@ -14,18 +14,24 @@ function generateLists(event) {
     let optionsDiv = document.createElement("div");
     let delBtn = document.createElement("a");
     let checkBtn = document.createElement("a");
+    let editBtn = document.createElement("a");
     delBtn.setAttribute("class", "del");
     checkBtn.setAttribute("class", "checkBtn");
+    editBtn.setAttribute("class", "editBtn");
     titleTask.innerHTML = `<span>${currentTask}.</span> ${taskInput.value}`;
     delBtn.innerHTML = `<i class="fa-solid fa-trash"></i>`;
     checkBtn.innerHTML = `<i class="fa-solid fa-check"></i>`;
+    editBtn.innerHTML = `<i class="fa-solid fa-pen-to-square"></i>`;
     list.appendChild(newTask);
     newTask.appendChild(titleTask);
     newTask.appendChild(optionsDiv);
+    optionsDiv.appendChild(editBtn);
     optionsDiv.appendChild(delBtn);
     optionsDiv.appendChild(checkBtn);
     delBtn.addEventListener("click", deleteTask);
     checkBtn.addEventListener("click", checkTask);
+    editBtn.addEventListener("click", editTask);
+    newTask.dataset.taskNumber = currentTask;
     taskInput.value = "";
     currentTask++;
   }
@@ -39,4 +45,14 @@ function deleteTask(event) {
 function checkTask(event) {
   const task = event.target.parentNode.parentNode.parentNode; 
   task.classList.toggle("check"); 
+}
+
+function editTask(event) {
+  event.preventDefault();
+  const task = event.target.parentNode.parentNode.parentNode;
+  let title = task.children[0];
+  const editText = prompt("Enter The Edit Text Task: ");
+  if (editText !== null) {
+    title.innerHTML = `<span>${task.dataset.taskNumber}.</span> ${editText}`;
+  }
 }
